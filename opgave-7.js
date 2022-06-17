a8b9b3b919efce88cf1d34cd7149c492e3a9ce8f
WA1YT-Tanish <112806@emmauscollege.nl>
2022-06-17 07:46:25 +0000

    ffffff

--- opgave-7.js
+++ opgave-7.js
@@ -6,12 +6,17 @@ module.exports = {gemiddelde}
    Ga ervan uit dat de parameter cijfers een array met numbers is.
    Hint: je hebt een for-loop en cijfers.length nodig
 */
-
+ //het goede antword is 8.75
 function gemiddelde(cijfers) {
   // cijfers is een array
   var uitkomst = undefined;
+  if(cijfers.length != 0){
+    uitkomst = 0;
   
-  //het goede antword is 8.75
-  
+  for(var teller = 0; teller < cijfers.length; teller = teller +1)
+    uitkomst =uitkomst + cijfers[teller]
+  }
+ 
+  uitkomst = uitkomst / cijfers.length;
   return uitkomst;
 }
